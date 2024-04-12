86524da60c8bf0b1311d49f39433c6e41c24f911
BHARATBHUSHAN NEGI <116365447+bhushannegi333@users.noreply.github.com>
2024-04-12 09:38:15 +0530

    Add files via upload

---
+++ Personal-portfolio-website/script.js
@@ -0,0 +1,20 @@
+function openDocument() {
+    // URL of the PDF document
+    var documentUrl = 'bhushan.pdf'; 
+    
+    // Open the document in a new window
+    window.open(documentUrl, '_blank');
+                            }
+        var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+               tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents){
+               tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+          document.getElementById(tabname).classList.add("active-tab");
+        }
